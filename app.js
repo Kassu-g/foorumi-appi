cc6f35193593f54c7e1de2c129d5ff739cd0b8e1
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-01-13 19:16:27 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./src/routes/user');
+const app = express();
+const P = 1234;
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/testdb')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+app.use('/api/user', userRoutes);
+app.listen(P, () => console.log(`Server running on http://localhost:${P}`));
---
+++ src/routes/user.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { email, password, username, isAdmin } = req.body;
+
+  // Check if email is already in use
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(403).json({ message: 'Email already in use' });
+
+  try {
+    const newUser = new User({ email, password, username, isAdmin });
+    await newUser.save();
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to register user' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return res.status(401).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, isAdmin: user.isAdmin },
+      process.env.SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to login' });
+  }
+});
+
+module.exports = router;
