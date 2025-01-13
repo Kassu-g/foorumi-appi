26cd3f482d0d5ec8c8a704fa415151002b5b64d7
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-01-13 21:10:46 +0200

    salaus

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const userSchema = new mongoose.Schema({
+const sala = require('bcryptjs');
+const skeema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   username: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
 });
 
-userSchema.pre('save', async function (next) {
+skeema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  const salt = await sala.genSalt(10);
+  this.password = await sala.hash(this.password, salt);
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', skeema);
