e32ba98a27a2660f5b4f90bdab0080559b0d9c73
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-01-15 20:15:05 +0200

    fgf

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const userRoutes = require('./src/routes/user');
+const rout = require('./src/routes/user');
 const app = express();
 const P = 1234;
 app.use(express.json());
@@ -14,5 +14,5 @@ mongoose.connect('mongodb://localhost:27017/testdb')
     console.error('Failed to connect to MongoDB', err);
   });
 
-app.use('/api/user', userRoutes);
+app.use('/api/user', rout);
 app.listen(P, () => console.log(`Server running on http://localhost:${P}`));
