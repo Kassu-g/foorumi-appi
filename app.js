088b788c4f74f5739dd3a591a8fda5101f080a7e
kasper666g <64731263+kasper666g@users.noreply.github.com>
2025-01-15 20:19:57 +0200

    fdgdf

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const rout = require('./src/routes/user');
 const app = express();
 const P = 1234;
 app.use(express.json());
-
 mongoose.connect('mongodb://localhost:27017/testdb')
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB!!');
   })
   .catch((err) => {
     console.error('Failed to connect to MongoDB', err);
   });
-
 app.use('/api/user', rout);
-app.listen(P, () => console.log(`Server running on http://localhost:${P}`));
+app.listen(P, () => console.log(`Server pelaa on http://localhost:${P}`));
